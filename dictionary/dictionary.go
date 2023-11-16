c21ab5f6d66012e3045a5cb466460029c3347160
haider jouini <haider.j@xelero.io>
2023-11-16 16:35:00 +0100

    Feat/Update dictionary functions

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,6 +1,11 @@
+// dictionary.go
+
 package dictionary
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 type Entry struct {
 	Definition string
@@ -10,22 +15,34 @@ func (e Entry) String() string {
 	return e.Definition
 }
 
+type dictionaryOperation struct {
+	word       string
+	definition string
+}
+
 type Dictionary struct {
-	entries map[string]Entry
+	entries   map[string]Entry
+	operation chan dictionaryOperation
+	mu        sync.RWMutex
 }
 
 func New() *Dictionary {
-	return &Dictionary{
-		entries: make(map[string]Entry),
+	d := &Dictionary{
+		entries:   make(map[string]Entry),
+		operation: make(chan dictionaryOperation),
 	}
+	go d.processOperations()
+	return d
 }
 
-func (d *Dictionary) Add(word string, definition string) {
-	entry := Entry{Definition: definition}
-	d.entries[word] = entry
+func (d *Dictionary) Add(word, definition string) {
+	d.operation <- dictionaryOperation{word, definition}
 }
 
 func (d *Dictionary) Get(word string) (Entry, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
 	entry, found := d.entries[word]
 	if !found {
 		return Entry{}, errors.New("word not found in the dictionary")
@@ -34,13 +51,33 @@ func (d *Dictionary) Get(word string) (Entry, error) {
 }
 
 func (d *Dictionary) Remove(word string) {
-	delete(d.entries, word)
+	d.operation <- dictionaryOperation{word, ""} // Empty definition signifies removal
 }
 
 func (d *Dictionary) List() ([]string, map[string]Entry) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
 	var words []string
 	for word := range d.entries {
 		words = append(words, word)
 	}
 	return words, d.entries
+}
+
+func (d *Dictionary) processOperations() {
+	for {
+		select {
+		case op := <-d.operation:
+			d.mu.Lock()
+			if op.definition == "" {
+				// If definition is empty, it's a removal operation
+				delete(d.entries, op.word)
+			} else {
+				// Otherwise, it's an addition operation
+				d.entries[op.word] = Entry{Definition: op.definition}
+			}
+			d.mu.Unlock()
+		}
+	}
 }
